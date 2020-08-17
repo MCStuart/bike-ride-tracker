3c42649c7f1eb972f91f9860020995fadaa9ee9a
MCStuart <47284075+MCStuart@users.noreply.github.com>
2020-08-17 12:07:59 -0700

    fix naming issues of components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from './components/navbar.component';
-import RideList from './components/ride-list.component';
+import RidesList from './components/ride-list.component';
 import EditRide from './components/edit-ride.component';
-import CreateRide from './components/create-exercise.component';
+import CreateRide from './components/create-ride.component';
 import CreateUser from './components/create-user.component';
 
 function App() {
@@ -14,10 +14,10 @@ function App() {
       <div className="container">
         <Navbar />
         <br />
-        <Route path='/' exact component={RideList} />
-        <Route path='/edit/:id' component={EditRide} />
-        <Route path='/create' component={CreateRide} />
-        <Route path='/user' component={CreateUser} />
+        <Route path="/" exact component={RidesList} />
+        <Route path="/edit/:id" component={EditRide} />
+        <Route path="/create" component={CreateRide} />
+        <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
