20b656caf066113d177a75d68915e6a59b3689cd
MCStuart <47284075+MCStuart@users.noreply.github.com>
2020-08-16 19:11:20 -0700

    add  find by id, update, delete ride routes

--- backend/routes/rides.js
+++ backend/routes/rides.js
@@ -4,8 +4,8 @@ const Ride = require('../models/ride.model');
 
 router.route('/').get((req, res) => {
   Ride.find()
-  .then(rides => res.json(rides))
-  .catch(err => res.status(400).json('Error: ' + err));
+    .then(rides => res.json(rides))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
@@ -24,8 +24,36 @@ router.route('/add').post((req, res) => {
   });
 
   newExercise.save()
-  .then(() => res.json('Exercise added!'))
-  .catch(err => res.status(400).json('Error: ' + err));
+    .then(() => res.json('Exercise added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req, res) => {
+  Ride.findById(req.params.id)
+    .then(ride => res.json(ride))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+  Ride.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Ride deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+  Ride.findByIdAndDelete(req.params.id)
+    .then(ride => {
+      ride.username = req.body.username;
+      ride.description = req.body.description;
+      ride.duration = Number(req.body.duration);
+      ride.distance = Number(req.body.distance);
+      ride.date = Date.parse(req.body.date);
+
+      ride.save()
+        .then(() => res.json('Ride updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
