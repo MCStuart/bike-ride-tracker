de131d0bfd43c8f873a458d643c2fb26961f29a5
MCStuart <47284075+MCStuart@users.noreply.github.com>
2020-08-25 10:56:48 -0700

    convert class component to functional

--- src/components/create-ride.component.js
+++ src/components/create-ride.component.js
@@ -1,168 +1,124 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
-export default class CreateRide extends Component {
-  constructor(props) {
-    super(props);
+const CreateRide = (props) => {
 
-    this.onChangeUsername = this.onChangeUsername.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeDuration = this.onChangeDuration.bind(this);
-    this.onChangeDistance = this.onChangeDistance.bind(this);
-    this.onChangeDate = this.onChangeDate.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+  const [username, setUsername] = useState('');
+  const [description, setDescription] = useState('');
+  const [duration, setDuration] = useState(0);
+  const [distance, setDistance] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
 
-    this.state = {
-      username: '',
-      description: '',
-      duration: 0,
-      distance: 0,
-      date: new Date(),
-      users: []
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:5000/users/');
+      if (response.data.length > 0) {
+        setUsers(response.data.map(user => user.username));
+        setUsername(response.data[0].username);
+      }
     }
-  }
+    fetchData();
+  }, []);
 
-  componentDidMount() {
-    axios.get('http://localhost:5000/users/')
-      .then(response => {
-        if (response.data.length > 0) {
-          this.setState({
-            users: response.data.map(user => user.username),
-            username: response.data[0].username
-          });
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
   }
-
-  onChangeUsername(e) {
-    this.setState({
-      username: e.target.value
-    });
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
   }
-
-  onChangeDescription(e) {
-    this.setState({
-      description: e.target.value
-    });
+  const onChangeDuration = (e) => {
+    setDuration(e.target.value);
   }
-
-  onChangeDuration(e) {
-    this.setState({
-      duration: e.target.value
-    });
+  const onChangeDistance = (e) => {
+    setDistance(e.target.value);
   }
-
-  onChangeDistance(e) {
-    this.setState({
-      distance: e.target.value
-    });
+  const onChangeDate = (date) => {
+    setDate(date);
   }
-
-  onChangeDate(date) {
-    this.setState({
-      date: date
-    });
-  }
-
-  onSubmit(e) {
+  const onSubmit = (e) => {
     e.preventDefault();
 
     const ride = {
-      username: this.state.username,
-      description: this.state.description,
-      duration: this.state.duration,
-      distance: this.state.distance,
-      date: this.state.date
+      username: username,
+      description: description,
+      duration: duration,
+      distance: distance,
+      date: date
     };
 
     console.log(ride);
 
     axios.post('http://localhost:5000/rides/add', ride)
-    .then(res => console.log(res.data));
+      .then(res => console.log(res.data));
 
     window.location = '/';
   }
 
+  return (
+    <div>
+      <h3>Create New Ride Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            required
+            className="form-control"
+            value={username}
+            onChange={onChangeUsername}>
+            {
+              users.map(user => {
+                return <option key={user} value={user}>{user}</option>;
+              })
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={onChangeDescription}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={duration}
+            onChange={onChangeDuration}
+          />
+        </div>
+        <div className="form-group">
+          <label>Distance (in miles): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={distance}
+            onChange={onChangeDistance}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={date}
+              onChange={onChangeDate}
+            />
+          </div>
+        </div>
 
-  render() {
-    return (
-      <div>
-        <h3>Create New Ride Log</h3>
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Username: </label>
-            <select ref="userInput"
-              required
-              className="form-control"
-              value={this.state.username}
-              onChange={this.onChangeUsername}>
-              {
-                this.state.users.map(function (user) {
-                  return <option
-                    key={user}
-                    value={user}>{user}
-                  </option>;
-                })
-              }
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Description: </label>
-            <input type="text"
-              required
-              className="form-control"
-              value={this.state.description}
-              onChange={this.onChangeDescription}
-            />
-          </div>
-          <div className="form-group">
-            <label>Duration (in minutes): </label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.duration}
-              onChange={this.onChangeDuration}
-            />
-          </div>
-          <div className="form-group">
-            <label>Distance (in miles): </label>
-            <input
-              type="text"
-              className="form-control"
-              value={this.state.distance}
-              onChange={this.onChangeDistance}
-            />
-          </div>
-          <div className="form-group">
-            <label>Date: </label>
-            <div>
-              <DatePicker
-                selected={this.state.date}
-                onChange={this.onChangeDate}
-              />
-            </div>
-          </div>
-
-          <div className="form-group">
-            <input type="submit" value="Create Ride Log" className="btn btn-primary" />
-          </div>
-        </form>
-      </div>
-    )
-  }
+        <div className="form-group">
+          <input type="submit" value="Create Ride Log" className="btn btn-primary" />
+        </div>
+      </form>
+    </div>
+  )
 }
 
-// const CreateRide = () => {
-//   return (
-//     <div>
-//       <p>Create ride component</p>
-//     </div>
-//   );
-// }
 
-// export default CreateRide;
+export default CreateRide; 
